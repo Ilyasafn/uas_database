c07d714aad80c9c62ce5e46e2a1cb7682e305508
Ilyas Al Furqon <ilyiyas05@gmail.com>
2024-09-17 19:57:40 +0800

    menambahkan routes pada post table

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import userRouter from "./routes/userRouter.js";
+import postRouter from "./routes/postRouter.js";
 
 const app = express();
 
 app.use(express.json());
 app.use("/user", userRouter);
+app.use("/post", postRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
---
+++ routes/postRouter.js
@@ -0,0 +1,29 @@
+import express from "express";
+import database from "../configs/database.js";
+
+const postRouter = express.Router();
+
+postRouter.get("/", async function (req, res) {
+  const [result, response] = await database.execute("SELECT * FROM post");
+  res.json({ posts: result });
+});
+
+postRouter.get("/:postId", async function (req, res) {
+  const postId = req.params.postId;
+  const [result, response] = await database.execute("SELECT id FROM post WHERE id = ?", [postId]);
+  res.json({ posts: result });
+});
+
+postRouter.post("/", async function (req, res) {
+  const { judul, tulisan, user_id } = req.body;
+  const [result, response] = await database.execute("INSERT INTO post (judul, tulisan, user_id) VALUES (?,?,?) ", [judul, tulisan, user_id]);
+  res.json({ result, message: "post baru berhasil ditambahkan" });
+});
+
+postRouter.delete("/:postId", async function (req, res) {
+  const postId = req.params.postId;
+  const [result, response] = await database.execute("DELETE FROM post WHERE id = ?", [postId]);
+  res.json({ result, message: "post berhasil dihapus" });
+});
+
+export default postRouter;
